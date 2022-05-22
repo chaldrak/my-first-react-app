0e159f4d24f33e7e40eff9b5931da366b4686b78
chaldrak <chaldrakus@gmail.com>
2022-05-23 00:03:05 +0100

    creation of the footer component

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import GlobalNav from './components/GlobalNav';
+import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
-import Button from './Button';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
     <GlobalNav />
-    
+    <Footer />
   </React.StrictMode>
 );
 
